1601aa0b7039924d1d36ea12e73f00265c945a43
eerohuusko <45799487+eerohuusko@users.noreply.github.com>
2021-11-26 13:47:04 +0200

    Update lisaa_tiedot.sql

--- mysql_injektio/lisaa_tiedot.sql
+++ mysql_injektio/lisaa_tiedot.sql
@@ -1,11 +1,11 @@
 -- skripti lisää neljä käyttäjätunnusta"ktunnukset" -tauluun.
 -- ensin annetaan ID numero, sitten käyttäjän nimi ja lopuksi salasana
 
-INSERT INTO ktunnukset
+INSERT INTO tunnukset
         VALUES ('1', 'eesimerkkii', 'salainen123');
-INSERT INTO ktunnukset
+INSERT INTO tunnukset
         VALUES ('2', 'jjokke', 'Passu');
-INSERT INTO ktunnukset
+INSERT INTO tunnukset
         VALUES ('3', 'iotti', 'Iotti123');
-INSERT INTO ktunnukset
+INSERT INTO tunnukset
         VALUES ('3', 'Hakkeri', 'supersecret');
