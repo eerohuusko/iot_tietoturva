bfaca82133cd8e691972d0676111cdb9a744e421
eerohuusko <45799487+eerohuusko@users.noreply.github.com>
2021-11-26 13:47:19 +0200

    Update luo_taulu.sql

--- mysql_injektio/luo_taulu.sql
+++ mysql_injektio/luo_taulu.sql
@@ -1,6 +1,6 @@
 -- luo taulun nimeltä ktunnukset
 -- taulu sisältää määritellyt kentät: id, kayttajanimi ja salasana
-CREATE TABLE ktunnukset
+CREATE TABLE tunnukset
 (
         id              int(5)          NOT NULL,
         kayttajanimi    CHAR(15)        NOT NULL,
